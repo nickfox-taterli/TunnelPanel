38877bba2aff7a61a442f4238aab3b885521b290
TaterLi <admin@taterli.com>
2021-07-14 18:02:19 +0800

    修正变量

--- resources/views/layouts/_header.blade.php
+++ resources/views/layouts/_header.blade.php
@@ -23,7 +23,7 @@
         @else
         <li class="nav-item dropdown">
             <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-            <img src="{{ $user->gravatar('140') }}" class="img-responsive img-circle" width="30px" height="30px">
+            <img src="{{ Auth::user()->gravatar('140') }}" class="img-responsive img-circle" width="30px" height="30px">
             {{ Auth::user()->name }}
             </a>
             <div class="dropdown-menu" aria-labelledby="navbarDropdown">
